b68cc8c5f02d9252f5268f8729475bd0709b9533
Grigory Shepelev <shegeley@gmail.com>
2024-12-28 23:03:46 +0300

    repl.lisp: proper order [critical fix]
    repl.lisp didn't load the project properly before. on
    `sly-eval-buffer` of hatis/core.lisp I always get "unknown package
    hatis.utils" althought it should be already loaded.
    
    finally fixed

--- repl.lisp
+++ repl.lisp
@@ -1,8 +1,9 @@
 ;; repl.lisp
 (require :asdf)
-
-(asdf/operate:require-system :slynk)
+(require :slynk)
+(require :uiop)
 
 (asdf:load-asd (merge-pathnames "hatis.asd" (uiop:getcwd)))
+(asdf:load-system :hatis)
 
 (slynk:create-server :port 4005 :dont-close t)
