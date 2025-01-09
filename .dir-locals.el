4ac848868380e7801b3cb813898379b2935f0cb6
Grigory Shepelev <shegeley@gmail.com>
2025-01-09 20:38:51 +0300

    .dir-locals.el: scheme -> common-lisp

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,12 +1,6 @@
-((scheme-mode
+((lisp-mode
   .
    ((geiser-mode-auto-p . nil)
     (indent-tabs-mode . nil)
-    (eval . (setq lisp-indent-offset 1))
-    (eval . (eval-after-load "arei"
-             ;; don't kill arei buffer when the process is dead; when dealing with SIGSEGV there might be some useful prints
-             '(defun arei-client--sentinel (process message)
-               "Called when connection is changed; in out case dropped."
-               (message "nREPL connection closed: %s" message)
-               ;; (kill-buffer (process-buffer process))
-               ))))))
+    (eval . (setq lisp-indent-offset 1
+                  lisp-body-indent 1)))))
